db7d64db691660967edd700fa4433e8241f9fc49
thuteshubham <thuteshubham@gmail.com>
2022-07-14 17:17:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const sql=require('mssql');
+
+var express = require('express');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+var app = express();
+const context=require('aws-lambda');
+const {routes:userRoutes}= require('./src/user/routes');
+
+// const config = {
+//     user: "admin",
+//     password: "Durga12345",
+//     server: "database-3.cxsg0qwb5pds.us-west-2.rds.amazonaws.com",
+//     database: "mssql2",
+//     pool: {
+//         idleTimeoutMillis: 60000
+//     },
+//     requestTimeout: 60000,
+//     options: {
+//         trustedconnection: true,
+//         enableArithAbort: true,
+//         instancename: 'SQLEXPRESS',
+//         encrypt: false // make false for local & make true for server database
+//     },
+//     port: 1433
+// }
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/user',userRoutes);
+
+app.listen(3000,console.log('server is listening oin port 3000'));
+
+
+
+
+// const dbConnection = new sql.ConnectionPool(config)
+// dbConnection.connect().then(pool => {
+//     console.log('Connected to MSSQL');
+//     return pool
+// })
+// .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
+
+// async function invokeDB(){
+//     const reponsee=await dbConnection.request().query('select * from users')
+//     console.log("reponsee",reponsee);
+// }
+
+
+
+module.exports=app
+
---
+++ src/user/routes.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const controller=require('./../controller/getAllTrains');
+
+const routes=express.Router({
+    mergeParams:true
+})
+
+routes.get('/',(req,res)=>{
+    res.status(200).json({});
+})
+
+routes.get('/users',controller.getAllTrains);
+routes.get('/activeInventory',controller.getActiveInventory);
+routes.get('/getDormantInventory',controller.getDormantInventory)
+routes.get('/getActiveLocoInfo',controller.getActiveLocoInfo)
+routes.get('/userData',controller.getUsersData);
+routes.post('/saveUser',controller.insertUserData);
+
+module.exports={
+    routes
+}
